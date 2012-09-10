4e43a4a039f626f91367822897ad9915f6119799
Brad Philips <brad.t.philips@gmail.com>
2012-09-09 21:09:55 -0400

    Clean up of last class.

--- code/LunchList/src/apt/tutorial/LunchListActivity.java
+++ code/LunchList/src/apt/tutorial/LunchListActivity.java
@@ -2,21 +2,15 @@ package apt.tutorial;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import android.app.TabActivity;
-import android.content.Context;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
-import android.widget.TabHost;
-import android.widget.TextView;
 
-public class LunchListActivity extends TabActivity {
+public class LunchListActivity extends Activity {
 	List<Restaurant> model = new ArrayList<Restaurant>();
 	RestaurantAdapter adapter = null;
 
@@ -26,44 +20,10 @@ public class LunchListActivity extends TabActivity {
 		setContentView(R.layout.main);
 		Button save = (Button) findViewById(R.id.save);
 		save.setOnClickListener(onSave);
-		
-		createResturants();
-		createTabControls();
-	}
-	
-	protected AdapterView.OnItemClickListener mAdapterOnClick = new AdapterView.OnItemClickListener() {
-		public void onItemClick(AdapterView<?> adapter, View view, int index, long id) {
-			Restaurant resturant = (Restaurant) adapter.getItemAtPosition(index); 
-		}
-	};
 
-	private void createTabControls() {
 		ListView list = (ListView) findViewById(R.id.restaurants);
-		adapter = new RestaurantAdapter(this);
+		adapter = new RestaurantAdapter(this, model);
 		list.setAdapter(adapter);
-
-		TabHost.TabSpec spec = getTabHost().newTabSpec("ListTab");
-		spec.setContent(R.id.restaurants);
-		spec.setIndicator("List", getResources().getDrawable(R.drawable.list));
-		getTabHost().addTab(spec);
-		
-		spec = getTabHost().newTabSpec("DetailsTab");
-		spec.setContent(R.id.details);
-		spec.setIndicator("Details", getResources().getDrawable(R.drawable.restaurant));
-		getTabHost().setCurrentTab(0);
-	}
-
-	private void createResturants() {
-		Restaurant bangkokThai = new Restaurant();
-		bangkokThai.setName("Bangkok Thai");
-		bangkokThai.setAddress("Mayfield Road");
-		
-		Restaurant austins = new Restaurant();
-		austins.setName("Austins");
-		austins.setAddress("Mayfield Road");
-		
-		model.add(bangkokThai);
-		model.add(austins);
 	}
 
 	private View.OnClickListener onSave = new View.OnClickListener() {
@@ -90,35 +50,5 @@ public class LunchListActivity extends TabActivity {
 			adapter.add(r);
 		}
 	};
-
-	static class RestaurantHolder {
-		private TextView name = null;
-		private TextView address = null;
-		private ImageView icon = null;
-
-		RestaurantHolder(View row) {
-			name = (TextView) row.findViewById(R.id.title);
-			address = (TextView) row.findViewById(R.id.address);
-			icon = (ImageView) row.findViewById(R.id.icon);
-		}
-
-		void populateFrom(Restaurant r, View row, Context mContext) {
-			name.setText(r.getName());
-			address.setText(r.getAddress());
-			if (r.getType().equals("sitdown")) {
-				icon.setImageResource(R.drawable.ball_red);
-				row.setBackgroundColor(mContext.getResources().getColor(
-						R.color.red));
-			} else if (r.getType().equals("takeout")) {
-				icon.setImageResource(R.drawable.ball_yellow);
-				row.setBackgroundColor(mContext.getResources().getColor(
-						R.color.yellow));
-			} else {
-				icon.setImageResource(R.drawable.ball_green);
-				row.setBackgroundColor(mContext.getResources().getColor(
-						R.color.green));
-			}
-		}
-	}
 
 }
--- code/LunchList/src/apt/tutorial/RestaurantAdapter.java
+++ code/LunchList/src/apt/tutorial/RestaurantAdapter.java
@@ -1,37 +1,50 @@
 package apt.tutorial;
 
-import android.app.Activity;
+import java.util.List;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import apt.tutorial.LunchListActivity.RestaurantHolder;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RestaurantAdapter extends ArrayAdapter<Restaurant> {
-	private Context mContext;
-	
-	public RestaurantAdapter(Context context) {
-		super(context, android.R.layout.simple_list_item_1);
-		mContext = context;
+
+	public RestaurantAdapter(Context context, List<Restaurant> model) {
+		super(context, android.R.layout.simple_list_item_1, model);
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View row = convertView;
-		RestaurantHolder holder;
-		
+		TextView name, address = null;
+		ImageView icon = null;
+
 		if (row == null) {
-			LayoutInflater inflater = ((Activity) getContext()).getLayoutInflater();
+			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			row = inflater.inflate(R.layout.row, null);
-		
-			holder = new RestaurantHolder(row);
-			row.setTag(holder);
-		} else {
-			holder = (RestaurantHolder) row.getTag();
 		}
 
-		holder.populateFrom((Restaurant) getItem(position), row, mContext);
+		name = (TextView) row.findViewById(R.id.title);
+		address = (TextView) row.findViewById(R.id.address);
+		icon = (ImageView) row.findViewById(R.id.icon);
+
+		Restaurant restaurant = getItem(position);
+		name.setText(restaurant.getName());
+		address.setText(restaurant.getAddress());
+		
+		if (restaurant.getType().equals("sitdown")) {
+			icon.setImageResource(R.drawable.ball_red);
+			row.setBackgroundColor(getContext().getResources().getColor(R.color.red));
+		} else if (restaurant.getType().equals("takeout")) {
+			icon.setImageResource(R.drawable.ball_yellow);
+			row.setBackgroundColor(getContext().getResources().getColor(R.color.yellow));
+		} else {
+			icon.setImageResource(R.drawable.ball_green);
+			row.setBackgroundColor(getContext().getResources().getColor(R.color.green));
+		}
 
 		return row;
 	}
